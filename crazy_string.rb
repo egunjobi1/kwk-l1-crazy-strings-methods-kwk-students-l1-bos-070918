1dc9100eb3f9bac92d5eea54f31f3cde4afbbcc2
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-10 17:13:47 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,7 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(first_string = "Step Aside", second_string = "Peasants")
+puts "#{first_string}.reverse, #{step aside}.reverse"
 end
+
+crazy_strings
